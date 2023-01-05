c01a0c145499f5a149f2283655ecbb5b17b3f388
shuvam <shubhamduttadnk@gmail.com>
2023-01-05 11:05:01 +0530

    classes

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,17 @@
 "use strict";
-console.log("Shuvam");
-let sname;
-sname = 'Shuvam';
+class Department {
+    constructor(n) {
+        this.name = n;
+    }
+    describe() {
+        console.log(this.name);
+    }
+}
+const accounting = new Department('Accounting');
+accounting.describe();
+const accountingCopy = {
+    name: 'Management',
+    describe: accounting.describe,
+    abcd: 'hiii'
+};
+accountingCopy.describe();
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,23 @@
-console.log("Shuvam");
-let sname:string;
-sname = 'Shuvam';
+class Department {
+  name: string;
+  constructor(n:string) {
+    this.name = n;
+  }
+
+  describe (this:Department){
+    console.log(this.name);
+  }
+}
+
+const accounting = new Department('Accounting');
+accounting.describe();
+const accountingCopy = {
+    name:'Management',
+    describe:accounting.describe,
+    abcd:'hiii'
+};
+accountingCopy.describe();
+
+
+
+
